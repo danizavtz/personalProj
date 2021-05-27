e69e043590b68ce57dc2761ad544a246d7936f69
Pires <daniel.pires@hp.com>
2021-05-27 17:01:49 -0300

    First endpoint call working

--- server/controllers/apolice.controller.js
+++ server/controllers/apolice.controller.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const tokenURLPrefix = ''
 const APIendpointInstance = axios.create({
     baseURL: process.env.CORPORATE_URL_ENDPOINT,
-    timeout: process.env.TIMEOUT,
+    timeout: parseInt(process.env.TIMEOUT),
     headers: {
         'Content-Type': 'application/json',
         'X-Company-Id': process.env.XCOMPANYID,
@@ -15,8 +15,21 @@ const APIendpointInstance = axios.create({
       }
 });
 
-exports.generateCode = (req, res, next) => {
-
+exports.generateCode = async (req, res, next) => {
+    body_data = {
+            "clientId":  process.env.CLIENT_ID,
+            "clientSecret": process.env.CLIENT_SECRET,
+            "grantType": process.env.G_TYPE
+    }
+    try {
+        const apicode = await APIendpointInstance.post('corporate/security/v1/authorize',body_data)
+        console.log(apicode)
+        req.apicode = apicode.data
+        next()
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({errors: [{ location: 'apolice', msg: 'There was an error on request for code token generation.', param: 'apolice' }]})
+    }
 }
 
 exports.generateToken = (req, res, next) => {
@@ -26,6 +39,6 @@ exports.generateToken = (req, res, next) => {
 
 
 exports.generateURL = (req, res) => {
-    res.status(200).json({ "msg": "ok" })
+    res.status(200).json(req.apicode)
 
 }
