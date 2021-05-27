c39ca755679c3e102d766c6d3962bd6e66b9d5e5
Pires <daniel.pires@hp.com>
2021-05-27 12:46:35 -0300

    initial project boilerplate

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+
+const app = express();
+cors({ credentials: true, origin: true });
+app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_ENV !== 'test') { app.use(logger('dev')); }
+
+app.use(require('./server/index'));
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    req.servermsg = {msg: "server up and running"}
+    res.status(200).json(req.servermsg);
+});
+  //após tentar casar todas as rotas a ultima rota que sobrou é not found
+router.get('*', (req, res) => {
+    res.status(404).json({ errors: [{location: req.path, msg: 'Not found', param: null}]});
+});
+
+module.exports = router;
