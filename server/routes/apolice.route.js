bd1841ff88338ae03b36e9fd8c76be190ab71ad2
Pires <daniel.pires@hp.com>
2021-05-27 15:51:46 -0300

    Fix values for routes and params

--- server/routes/apolice.route.js
+++ server/routes/apolice.route.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const apoliceController = require('../controllers/apolice.controller');
 const apoliceValidator = require('../validators/apolice.validator');
 
-router.get('/apolice/:id', apoliceValidator.validationParamsRules, apoliceValidator.checkRules);
+router.get('/apolice/:id', apoliceValidator.validationParamsRules, apoliceValidator.checkRules, apoliceController.generateURL);
 
 module.exports = router;
